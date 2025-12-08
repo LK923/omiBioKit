49afa5db837bd997debf29f839557c032851631f
Oceanmist <linkaiwen048@gmail.com>
2025-12-07 21:43:24 -0500

    update demo

--- omibio/__init__.py
+++ omibio/__init__.py
@@ -1,6 +1,8 @@
 from importlib.metadata import version
 from omibio.sequence import Sequence, Polypeptide
-from omibio.bio import SeqInterval, Gene, Genome
+from omibio.bio import (
+    SeqInterval, Gene, Genome, SeqCollections, SeqEntry, AnalysisResult
+)
 from omibio.sequence.seq_utils.clean import CleanReport, CleanReportItem
 from omibio.io.read_fasta import FastaFormatError
 
@@ -9,6 +11,7 @@ __version__ = version("omibio")
 __all__ = [
     "Sequence", "Polypeptide",
     "SeqInterval", "Gene", "Genome",
+    "SeqCollections", "SeqEntry", "AnalysisResult",
     "CleanReport", "CleanReportItem",
     "FastaFormatError"
 ]
